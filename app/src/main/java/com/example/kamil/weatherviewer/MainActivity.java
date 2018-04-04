311ece925c63065e260cde78d91ebad71ff27a5e
KamilSobien <kamil.sobien@gmail.com>
2018-04-05 00:19:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kamil/weatherviewer/MainActivity.java
@@ -0,0 +1,175 @@
+package com.example.kamil.weatherviewer;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<Weather> weatherList = new ArrayList<>();
+
+    private WeatherArrayAdapter weatherArrayAdapter;
+    private ListView weatherListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        weatherListView = findViewById(R.id.weatherListView);
+        weatherArrayAdapter = new WeatherArrayAdapter(this, weatherList);
+        weatherListView.setAdapter(weatherArrayAdapter);
+
+        EditText locationEditText = findViewById(R.id.locationEditText);
+        locationEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(i == KeyEvent.KEYCODE_ENTER ){
+                    EditText locationEditText = findViewById(R.id.locationEditText);
+                    loadWeather(locationEditText);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText locationEditText = findViewById(R.id.locationEditText);
+                loadWeather(locationEditText);
+            }
+        });
+    }
+
+    private void loadWeather(EditText locationEditText) {
+        URL url = createURL(locationEditText.getText().toString());
+
+        if (url != null) {
+            dismissKeyboard(locationEditText);
+            GetWeatherTask getLocalWeatherTask = new GetWeatherTask();
+            getLocalWeatherTask.execute(url);
+        } else {
+            Snackbar.make(findViewById(R.id.coordinatorLayout),
+                    R.string.invalid_url, Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    private void dismissKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        if(imm != null && imm.isAcceptingText()) { // verify if the soft keyboard is open
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private URL createURL(String city) {
+        String apiKey = getString(R.string.api_key);
+        String baseURL = getString(R.string.web_service_url);
+
+        try {
+            String urlString = baseURL + URLEncoder.encode(city, "UTF-8")
+                    + ",pl"
+                    + "&units=metric&lang=pl&cnt=16&APPID=" + apiKey;
+            return new URL(urlString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private class GetWeatherTask extends AsyncTask<URL, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(URL... urls) {
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) urls[0].openConnection();
+                int response = connection.getResponseCode();
+                if (response == HttpURLConnection.HTTP_OK) {
+                    StringBuilder builder = new StringBuilder();
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            builder.append(line);
+                        }
+                    } catch (IOException e) {
+                        Snackbar.make(findViewById(R.id.coordinatorLayout),
+                                R.string.read_error, Snackbar.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                    return new JSONObject(builder.toString());
+                } else {
+                    Snackbar.make(findViewById(R.id.coordinatorLayout),
+                            R.string.connect_error, Snackbar.LENGTH_LONG).show();
+                }
+            } catch (Exception e) {
+                Snackbar.make(findViewById(R.id.coordinatorLayout),
+                        R.string.connect_error, Snackbar.LENGTH_LONG).show();
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject weather) {
+            convertJSONtoArrayList(weather);// repopulate weatherList
+            weatherArrayAdapter.notifyDataSetChanged();// rebind to ListView
+            weatherListView.smoothScrollToPosition(0);  // scroll to top
+        }
+
+        private void convertJSONtoArrayList(JSONObject forecast) {
+            weatherList.clear();
+
+            if(forecast== null)
+                return;
+
+            try {
+                JSONArray list = forecast.getJSONArray("list");
+                for (int i = 0; i < list.length(); i++) {
+                    JSONObject day = list.getJSONObject(i);
+                    JSONObject temperatures = day.getJSONObject("main");
+                    JSONObject weather = day.getJSONArray("weather").getJSONObject(0);
+                    weatherList.add(
+                            new Weather(
+                                    day.getLong("dt"),
+                                    temperatures.getDouble("temp_min"),
+                                    temperatures.getDouble("temp_max"),
+                                    temperatures.getDouble("humidity"),
+                                    weather.getString("description"),
+                                    weather.getString("icon")));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
